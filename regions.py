0a29ed945f84e40de078c17ce574829c40f1ffee
Scotasloth <rossmcc76@gmail.com>
2026-06-09 13:57:31 +0100

    Multiworld now generates, not real options yet

--- regions.py
+++ regions.py
@@ -1,4 +1,4 @@
-from BaseClasses import Region, Entrance
+from BaseClasses import Region, Location
 
 
 def create_regions(world):
@@ -6,38 +6,9 @@ def create_regions(world):
     player = world.player
 
     menu = Region("Menu", player, multiworld)
+
+    # ✔ create real location
+    loc = Location(player, "Test Location 1", 1, menu)
+    menu.locations.append(loc)
+
     multiworld.regions.append(menu)
-
-    levels = [
-        "Turtle Woods",
-        "Snow Go",
-        "Hang Eight",
-        "The Pits",
-        "Crash Dash",
-        "Air Crash",
-        "Bear It"
-    ]
-
-    previous = menu
-
-    for name in levels:
-        region = Region(name, player, multiworld)
-        multiworld.regions.append(region)
-
-        entrance = Entrance(f"{previous.name} -> {name}", previous)
-
-        # ✅ ONLY THIS IS REQUIRED
-        entrance.connect(region)
-
-        previous.exits.append(entrance)
-
-        previous = region
-
-    final = Region("Final Area", player, multiworld)
-    multiworld.regions.append(final)
-
-    entrance = Entrance(f"{previous.name} -> Final Area", previous)
-
-    entrance.connect(final)
-
-    previous.exits.append(entrance)
